9a75321a680ea804765612e0d1d3530d1466b489
Jean-Michel Leclercq <Jean-Michel@Jean-Michel-PC.(none)>
2011-10-29 17:32:50 +0200

    DBTable

---
+++ application/models/DbTable/GameResult.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_GameResult extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'game_result';
+
+
+}
+
---
+++ application/models/DbTable/Registration.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Registration extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'registration';
+
+
+}
+
