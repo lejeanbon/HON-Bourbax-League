1f8349124c2f8c5ca5c7c5da1e33a934c7b124f6
Jean-Michel Leclercq <Jean-Michel@Jean-Michel-PC.(none)>
2011-10-29 17:23:29 +0200

    layout + config

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,44 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap{
 
+    protected function _initSession(){
+        $session = new Zend_Session_Namespace('sdis35', true);
+        return $session;
+    }
+
+    protected function _initDb(){
+        $config = new Zend_Config($this->getOptions());
+        // Mise en place de la BDD
+        $oDb = Zend_Db::factory($config->resources->db);
+        $oDb->query('SET NAMES UTF8');
+        // on définit notre objet de base de données par défaut
+        Zend_Db_Table::setDefaultAdapter($oDb);
+    }
+
+    protected function _initView(){
+        // Initialisons la vue
+        $view = new Zend_View();
+        Zend_Layout::startMvc();
+        $view->doctype('XHTML1_STRICT');
+        $view->headTitle('SDIS35 - Gestion des stages/formations');
+        // Ajoutons là au ViewRenderer
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+        $viewRenderer->setView($view);
+        // Retourner la vue pour qu'elle puisse être stockée par le bootstrap
+        return $view;
+    }
+
+    protected function _initAutoload(){
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+        'namespace' => '',
+        'basePath' => APPLICATION_PATH));
+        return $moduleLoader;
+    }
+
+    protected  function  _initViewHelpers(){
+        $this->bootstrap('layout');
+    }
 
 }
 
